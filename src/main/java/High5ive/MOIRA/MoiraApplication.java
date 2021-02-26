bfd70c5e5aa90e2ae0b69de3d3c27744588f8787
welshimeat <kimsw1726@naver.com>
2021-02-26 22:47:44 +0900

    first commit

---
+++ src/main/java/High5ive/MOIRA/MoiraApplication.java
@@ -0,0 +1,13 @@
+package High5ive.MOIRA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/High5ive/MOIRA/MoiraApplicationTests.java
@@ -0,0 +1,13 @@
+package High5ive.MOIRA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
