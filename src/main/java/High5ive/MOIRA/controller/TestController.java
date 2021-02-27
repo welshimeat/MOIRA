340ab9cfbf52a191a9ef246972e31beb1ccffcc4
welshimeat <kimsw1726@naver.com>
2021-02-27 12:28:16 +0900

    test deploy

---
+++ src/main/java/High5ive/MOIRA/controller/TestController.java
@@ -0,0 +1,12 @@
+package High5ive.MOIRA.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String getTest(){
+        return "test";
+    }
+}
